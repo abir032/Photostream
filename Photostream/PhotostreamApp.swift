3c151aebb8d4aa5c7e9643e09ff7e8e5e668db8b
Md Fahim Faez Abir <mdfahimfaezabir@Abir.local>
2025-09-03 20:23:55 +0600

    chore: initialize repository
    This is an initial commit for project creation
    
    Added .gitignore template
    
    refs: #001

---
+++ Photostream/PhotostreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotostreamApp.swift
+//  Photostream
+//
+//  Created by Md Fahim Faez Abir on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotostreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotostreamTests/PhotostreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotostreamTests.swift
+//  PhotostreamTests
+//
+//  Created by Md Fahim Faez Abir on 3/9/25.
+//
+
+import Testing
+@testable import Photostream
+
+struct PhotostreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
