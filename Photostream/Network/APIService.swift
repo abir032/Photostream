290e17c4be61de3bfb611e26f1c44982c0b96445
Md Fahim Faez Abir <mdfahimfaezabir@Abir.local>
2025-09-05 20:00:36 +0600

    chore: Add api service class

--- Photostream/Network/APIService.swift
+++ Photostream/Network/APIService.swift
@@ -5,3 +5,75 @@
 //  Created by Md Fahim Faez Abir on 4/9/25.
 //
 
+import Foundation
+
+
+class APIService {
+    static let shared = APIService()
+    private let networkMonitor = NetworkMonitor.shared
+    private let cache = NSCache<NSString, NSData>()
+    private var session = URLSession.shared
+
+    private init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .useProtocolCachePolicy
+        URLCache.shared = URLCache(
+            memoryCapacity: 50_000_000,
+            diskCapacity: 200_000_000,
+            diskPath: "APIServiceCache"
+        )
+        configuration.urlCache = URLCache.shared
+        self.session = URLSession(configuration: configuration)
+    }
+
+    func fetchData<T: Codable>(from urlString: String) async throws -> T {
+        guard await networkMonitor.checkNetworkConnection() else {
+            return try fetchFromCache(for: urlString)
+        }
+
+        guard let url = URL(string: urlString) else {
+            throw AppError.invalidURL
+        }
+
+        if let cachedResult: T = try? fetchFromCache(for: urlString) {
+            return cachedResult
+        }
+
+        let (data, response) = try await session.data(from: url)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw AppError.invalidResponse
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw AppError.serverError(statusCode: httpResponse.statusCode)
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let result = try decoder.decode(T.self, from: data)
+            saveToCache(data: data, for: urlString)
+            return result
+        } catch {
+            throw AppError.decodingError
+        }
+    }
+
+    private func saveToCache(data: Data, for urlString: String) {
+        cache.setObject(data as NSData, forKey: urlString as NSString)
+    }
+
+    private func fetchFromCache<T: Codable>(for urlString: String) throws -> T {
+        guard let cachedData = cache.object(forKey: urlString as NSString) as Data? else {
+            throw AppError.noData
+        }
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(T.self, from: cachedData)
+        } catch {
+            throw AppError.decodingError
+        }
+    }
+}
